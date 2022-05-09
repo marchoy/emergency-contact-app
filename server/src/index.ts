3b02692d90f9d3fec300148e7b9ee107f99a8fc5
Marc Hoyle <marchoyle27@gmail.com>
2022-05-08 22:54:33 -0700

    build: initialize server
    Build framework for server.
    - TypeScript
    - Express
    - TypeORM
    Test resolver with HelloResolver.

---
+++ server/src/index.ts
@@ -0,0 +1,52 @@
+import "reflect-metadata"; // typeorm
+import "dotenv/config";
+import { createConnection } from "typeorm";
+import path from "path";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+import cors from "cors";
+
+const main = async () => {
+    const connection = await createConnection({
+        type: "postgres",
+        database: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        logging: true,
+        synchronize: true,
+        migrations: [path.join(__dirname, "./migrations/*")],
+        // entities: [Post, User, Updoot],
+    });
+
+    // await connection.runMigrations();
+
+    const app = express();
+
+    app.use(
+        cors({
+            origin: ["http://localhost:3000", "https://studio.apollographql.com"],
+            credentials: true,
+        })
+    );
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false,
+        }),
+    });
+
+    await apolloServer.start();
+    apolloServer.applyMiddleware({
+        app,
+        cors: false,
+    });
+
+    app.listen(4000, () => {
+        console.log("🚀 Server ready at http://localhost:4000/");
+    });
+};
+
+main();
