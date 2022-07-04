7c824e977bd4ee73b18f831e82675621dceeaf4e
Marc Hoyle <marchoyle27@gmail.com>
2022-07-04 14:40:04 -0700

    feat: add cache update for `createPhoneNumber` mutation

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -4,25 +4,38 @@ import theme from '../theme';
 import { AppProps } from 'next/app';
 import { createClient, dedupExchange, fetchExchange, Provider } from 'urql';
 import { cacheExchange } from '@urql/exchange-graphcache';
+import { ContactNumbersDocument, CreatePhoneNumberMutation, MutationCreatePhoneNumberArgs } from '../generated/graphql';
 
 const client = createClient({
   url: process.env.NEXT_PUBLIC_API_URL,
-  exchanges: [dedupExchange, cacheExchange({
-    updates: {
-      Mutation: {
-        deleteContact: (result, args, cache, info) => {
-          cache.invalidate({ __typename: "Contact", id: args.id as number });
-        },
-        createContact: (result, args, cache, info) => {
-          // invalidate the cache and re-fetch
-          cache.invalidate("Query", "contacts");
-        },
-        deletePhoneNumber: (result, args, cache, info) => {
-          cache.invalidate({ __typename: "PhoneNumber", id: args.id as number });
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          deleteContact: (result, args, cache, info) => {
+            cache.invalidate({ __typename: "Contact", id: args.id as number });
+          },
+          createContact: (result, args, cache, info) => {
+            // invalidate the cache and re-fetch
+            cache.invalidate("Query", "contacts");
+          },
+          deletePhoneNumber: (result, args, cache, info) => {
+            cache.invalidate({ __typename: "PhoneNumber", id: args.id as number });
+          },
+          createPhoneNumber: (result, args, cache, info) => {
+            const { createPhoneNumber } = result as CreatePhoneNumberMutation;
+            const { input: { contactId } } = args as MutationCreatePhoneNumberArgs;
+            cache.updateQuery({ query: ContactNumbersDocument, variables: { contactId } }, (data: any) => {
+              data?.contact?.contactNumbers?.push(createPhoneNumber);
+              return data;
+            });
+          },
         },
       },
-    },
-  }) as any, fetchExchange],
+    }) as any,
+    fetchExchange
+  ],
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
